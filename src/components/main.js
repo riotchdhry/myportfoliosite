d79b43f42c076eeb3bdda15f55838e2ed5ed8735
riotchdhry <rohittchoudhary@outlook.com>
2020-12-10 20:56:02 -0500

    main.js GA implemented

--- src/components/main.js
+++ src/components/main.js
@@ -6,7 +6,13 @@ import LandingPage from './landingpage';
 import Contact from './contact';
 import Resume from './resume';
 import Projects from './projects';
+import ReactGA from 'react-ga'
+import { useEffect } from 'react';
 
+useEffect(() => {
+    ReactGA.initialize('TRACKING ID');
+    ReactGA.pageview(window.location.pathname + window.location.search)
+},[]);
 
 const Main = () =>(
     <Switch>
